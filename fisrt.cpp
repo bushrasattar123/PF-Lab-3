e4dc72797703e34efcd8368806e1a9a227d9bfa6
bushrasattar123 <120782351+bushrasattar123@users.noreply.github.com>
2024-09-05 12:06:53 +0500

    Add files via upload

---
+++ fisrt.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	char ch='A';
+	printf("the character data is %c\n",ch);
+	int a;
+	int b;
+	int c;
+	printf("Enter value of a\n");
+	scanf("%d", &a);
+	printf("Enter value of b\n");
+	scanf("%d", &b);
+	c=a+b;
+	printf("the sum of a and b is %d\n", c);
+	
+	float f=3.14;
+	printf("you entered a float value %f\n",f);
+}
+
